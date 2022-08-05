662ede6632d706b958f38b22fd3aa94fad8401d1
Rafael Collado <rco@fb.com>
2022-08-04 23:52:59 -0700

    refactoring express/react/heroku

--- index.js
+++ index.js
@@ -39,33 +39,8 @@ app.listen(port, () => {
   console.log(`🚀 Parse app listening on port ${port}`);
 });
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
-
-// The "catchall" handler: for any request that isn't an api request, 
-// send back React's index.html file to render the webpage.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
-
-
-
-
 // **********************************************************************
-// ENDPOINTS
+// ENDPOINTS - Put all API endpoints under '/api'
 // **********************************************************************
 
 // create user account
@@ -162,8 +137,8 @@ app.post('/api/upload', upload.single('file'), async (req, res) => {
   }
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// The "catchall" handler: for any request that isn't an api request, 
+// send back React's index.html file to render the webpage.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
