010e750e97ad6851f07e56e760948115da1ab646
Rafael Collado <rco@fb.com>
2022-08-05 01:25:48 -0700

    debug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 /* eslint-disable */
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      node: true,
     },
-    ecmaVersion: 'latest',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    'no-unused-vars': 'off',
-    'no-console': 'off',
-    'react/prop-types': 'off',
-  },
-};
+    extends: [
+      'airbnb',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 'latest',
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    rules: {
+      'no-unused-vars': 'off',
+      'no-console': 'off',
+      'react/prop-types': 'off',
+    },
+  };
+  
