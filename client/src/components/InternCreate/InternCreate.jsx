93891014f024795f90f659de12fae2a65e5dff04
Rafael Collado <rco@fb.com>
2022-08-09 09:51:21 -0700

    heroku debugging

--- client/src/components/InternCreate/InternCreate.jsx
+++ client/src/components/InternCreate/InternCreate.jsx
@@ -165,6 +165,9 @@ export default function InternCreate({
         onClick={handleOnInternInfoSubmit}
       />
       <h3>{error}</h3>
+      <script
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`}
+      />
     </div>
   );
 }
